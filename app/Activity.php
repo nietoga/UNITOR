9405cfba51debfd40fee396056a56f0bc38956fa
Kevin Gutierrez Gomez <kevingtzz@outlook.com>
2020-03-07 14:14:28 -0500

    modelo y migraciones

---
+++ app/Activity.php
@@ -0,0 +1,93 @@
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Activity extends Model
+{
+    /**
+     * Mass assignable fields of Activity model
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'activity_id',
+        'name',
+        'grade',
+        'porcentage'
+    ];
+
+    /**
+     * Returns current Activity id
+     *
+     * @return int
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Returns the name of the Activity
+     *
+     * @return string
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+    /**
+     * Sets the name of the Activity
+     *
+     * @param string $name
+     * @return void
+     */
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    /**
+     * Returns the grade of the Activity
+     *
+     * @return float
+     */
+    public function getGrade() {
+        return $this->grade;
+    }
+
+    /**
+     * Sets the grade of the Activity
+     *
+     * @param float $grade
+     * @return void
+     */
+    public function setGrade($grade) {
+        $this->grade = $grade;
+    }
+
+    /**
+     * Returns the porcentage of the Activity
+     *
+     * @return float
+     */
+    public function getPorcentage() {
+        return $this->porcentage;
+    }
+
+    /**
+     * Sets the porcentage of the Activity
+     *
+     * @param float $porcentage
+     * @return void
+     */
+    public function setPorcentage($porcentage) {
+        $this->porcentage = $porcentage;
+    }
+
+    /**
+     * Returns the Course that owns this Activity
+     *
+     * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function period() {
+        return $this->belongsTo(Course::class);
+    }
+}
---
+++ database/migrations/2020_03_07_190037_create_activity_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activity', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('course_id')->unsigned();
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->string('name');
+            $table->float('grade');
+            $table->float('porcentage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activity');
+    }
+}
