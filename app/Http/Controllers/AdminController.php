803076580b792662639e9c6aab428629fb6fb792
Agustín Nieto <anietog1@eafit.edu.co>
2020-03-10 09:25:09 -0500

    Now allowing only admins to see admin views

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,6 +9,7 @@ class AdminController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('is_admin');
     }
 
     public function index()
