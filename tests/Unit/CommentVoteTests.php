58363bd6b30d8680dae06d3f53fcc615da6f648d
Agustín Nieto <anietog1@eafit.edu.co>
2020-03-16 19:29:02 -0500

    Adding missing test

---
+++ tests/Unit/CommentVoteTests.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\CommentVote;
+use PHPUnit\Framework\TestCase;
+
+class CommentVoteTests extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $commentVote = new CommentVote([
+            'comment_id' => 2,
+            'user_id' => 3,
+            'vote_type' => 'added',
+        ]);
+
+        $this->assertTrue($commentVote->getCommentId() == 2);
+        $this->assertTrue($commentVote->getUserId() == 3);
+        $this->assertTrue($commentVote->getVoteType() == 'added');
+
+        $commentVote->setVoteType('subtracted'); // there are no other collateral effects
+        $this->assertTrue($commentVote->getCommentId() == 2);
+        $this->assertTrue($commentVote->getUserId() == 3);
+        $this->assertTrue($commentVote->getVoteType() == 'subtracted');
+    }
+}
