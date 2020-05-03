cb4fb6a98424c5d17cbfd49da1fe30eb65fe34b7
Kevin Gutierrez Gomez <kevingtzz@outlook.com>
2020-05-02 21:58:55 -0500

    main page stylized

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,23 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{__('messages.main')}}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-                    <p>{{ __('messages.welcome') }}</p>
-                    <a class="btn btn-primary" href="{{ route('period.index') }}">{{__('messages.periods')}}</a>
-                    @if(Auth::user()->isAdmin())
-                    <a class="btn btn-primary" href="{{ route('admin.index') }}">{{__('messages.panel')}}</a>
-                    @endif
-                </div>
+        <div class="col-md-10 text-center">
+            <div id="main-text" class="card-header">{{__('messages.main')}}</div>
+            @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
             </div>
+            @endif
+            <p id="message_welcome">{{ __('messages.welcome') }}</p>
+            <a class="btn btn-primary btn-lg m-3" href="{{ route('period.index') }}">{{__('messages.periods')}}</a>
+            @if(Auth::user()->isAdmin())
+            <a class="btn btn-primary btn-lg" href="{{ route('admin.index') }}">{{__('messages.panel')}}</a>
+            @endif
         </div>
     </div>
 </div>
